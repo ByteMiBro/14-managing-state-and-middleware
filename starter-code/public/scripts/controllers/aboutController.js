c504f87311be799ca087401a8194a3b32422670a
ByteMiBro <bytemibro@gmail.com>
2017-07-27 14:05:20 -0500

    comment in aboutController.js

--- starter-code/public/scripts/controllers/aboutController.js
+++ starter-code/public/scripts/controllers/aboutController.js
@@ -5,7 +5,7 @@ var app = app || {};
   const aboutController = {};
 
   // COMMENT: What is this function doing? Where is it called? Does it call any other functions, and if so, in what file(s) do those function(s) live?
-  // (put your response in a comment here)
+  //In the index.html this finds the id of about and shows what ever is attached to the id about, which in this case is the articles, and then finds the siblings and hides them, then calls the function that is in the repos.js file. This function gets called as response in routes.
   aboutController.index = () => {
     $('#about').show().siblings().hide();
     app.repos.requestRepos(app.repoView.index);
