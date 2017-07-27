f07681437d3d1f9f59485e9758713479673ab4e3
nerdbetter <jasontlogan@gmail.com>
2017-07-27 15:21:51 -0500

    added comments to adminView.js

--- starter-code/public/scripts/views/adminView.js
+++ starter-code/public/scripts/views/adminView.js
@@ -6,6 +6,7 @@ var app = app || {};
     initAdminPage : () => {
       let template = Handlebars.compile($('#author-template').text());
       // COMMENT: What is this function doing? Where is it called? Does it call any other functions, and if so, in what file(s) do those function(s) live?
+      // These line handle the appending of the data that is returned from the numWordsByAuthor, Article.all.length, and numWordsAll functions in the article.js file to the handlebars template in the spots they belong.
       app.Article.numWordsByAuthor().forEach(stat => $('.author-stats').append(template(stat)));
       $('#blog-stats .articles').text(app.Article.all.length);
       $('#blog-stats .words').text(app.Article.numWordsAll());
