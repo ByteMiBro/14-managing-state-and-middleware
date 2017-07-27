0b350607d0dcec018b0e05f70898fa57c8a131b9
ByteMiBro <bytemibro@gmail.com>
2017-07-27 14:37:57 -0500

    repoView.js

--- starter-code/public/scripts/views/repoView.js
+++ starter-code/public/scripts/views/repoView.js
@@ -14,6 +14,7 @@ var app = app || {};
   const render = Handlebars.compile($('#repo-template').text());
 
   // COMMENT: What is this function doing? Where is it called? Does it call any other functions, and if so, in what file(s) do those function(s) live?
+  //Calls the ui function, then appends the any ul descendents with any repo object with name and renders it by going over an repos.all array and returns all objects in the repo.all array whith the property of name.
   repoView.index = function() {
     ui();
 
